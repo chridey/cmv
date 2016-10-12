86ace36a503cc9e9f606671c827b116742ee164a
Chris Hidey <chidey@cs.columbia.edu>
2016-10-12 11:10:58 -0400

    updating rnn preprocessing to allow for ignoring unseen words in testing

--- cmv/rnn/preprocessing.py
+++ cmv/rnn/preprocessing.py
@@ -1,7 +1,7 @@
 MAX_POST_LENGTH = 40
 MAX_SENTENCE_LENGTH = 256
 
-def make_sentence_indices(sentence, indices, max_sentence_length):
+def make_sentence_indices(sentence, indices, max_sentence_length, add=True):
     sentence_indices = []
     for word in sentence:
         #word = word.lower()
@@ -9,17 +9,19 @@ def make_sentence_indices(sentence, indices, max_sentence_length):
         if not len(word):
             continue
         if word not in indices:
+            if not add:
+                continue
             indices[word] = len(indices)
         sentence_indices.append(indices[word])
 
     #pad out to max sentence length
     return sentence_indices[:max_sentence_length] + [0]*max(0, max_sentence_length-len(sentence_indices))
 
-def make_post_indices(post, indices, max_sentence_length, max_post_length):
+def make_post_indices(post, indices, max_sentence_length, max_post_length, add=True):
     post_indices = []
 
     for sentence in post:
-        sentence_indices = make_sentence_indices(sentence, indices, max_sentence_length)
+        sentence_indices = make_sentence_indices(sentence, indices, max_sentence_length, add)
         if len(sentence_indices):
             post_indices.append(sentence_indices)
 
@@ -40,7 +42,7 @@ def make_sentence_mask(post_length, max_post_length, sentence_lengths, max_sente
                                         
 def build_indices(op, pos, neg, indices=None, mask=False,
                   max_sentence_length=MAX_SENTENCE_LENGTH,
-                  max_post_length=MAX_POST_LENGTH):
+                  max_post_length=MAX_POST_LENGTH, add=True):
     if not indices:
         indices = {None:0}
 
@@ -53,7 +55,7 @@ def build_indices(op, pos, neg, indices=None, mask=False,
     gold = []
 
     for i in range(len(op)):
-        curr_indices = make_post_indices(op[i], indices, max_sentence_length, max_post_length)
+        curr_indices = make_post_indices(op[i], indices, max_sentence_length, max_post_length, add)
         if mask:
             indices_mask_s = make_sentence_mask(len(curr_indices), max_post_length,
                                                 [len(j) for j in curr_indices], max_sentence_length)
@@ -62,7 +64,7 @@ def build_indices(op, pos, neg, indices=None, mask=False,
             op_mask.extend([indices_mask, indices_mask])
         op_ret.extend([curr_indices, curr_indices])
 
-        curr_indices = make_post_indices(pos[i], indices, max_sentence_length, max_post_length)
+        curr_indices = make_post_indices(pos[i], indices, max_sentence_length, max_post_length, add)
         if mask:
             indices_mask_s = make_sentence_mask(len(curr_indices), max_post_length,
                                                 [len(j) for j in curr_indices], max_sentence_length)
@@ -71,7 +73,7 @@ def build_indices(op, pos, neg, indices=None, mask=False,
             resp_mask.append(indices_mask)
         resp_ret.append(curr_indices)
     
-        curr_indices = make_post_indices(neg[i], indices, max_sentence_length, max_post_length)
+        curr_indices = make_post_indices(neg[i], indices, max_sentence_length, max_post_length, add)
         if mask:
             indices_mask_s = make_sentence_mask(len(curr_indices), max_post_length,
                                                 [len(j) for j in curr_indices], max_sentence_length)
